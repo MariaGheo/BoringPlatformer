1bc3ea8ee8769510d55d8fbdf6adfed9c82e8253
maria <maria@DESKTOP-V4PMJTL>
2022-06-06 14:44:10 -0400

    Add project files.

---
+++ BoringPlatformer/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BoringPlatformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.troubleshootLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // troubleshootLabel
+            // 
+            this.troubleshootLabel.AutoSize = true;
+            this.troubleshootLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.troubleshootLabel.ForeColor = System.Drawing.Color.White;
+            this.troubleshootLabel.Location = new System.Drawing.Point(12, 106);
+            this.troubleshootLabel.Name = "troubleshootLabel";
+            this.troubleshootLabel.Size = new System.Drawing.Size(160, 24);
+            this.troubleshootLabel.TabIndex = 0;
+            this.troubleshootLabel.Text = "troubleshootLabel";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(302, 30);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(160, 20);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "scoreLabel";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(251, 144);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(250, 30);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "titleLabel";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(174, 221);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(400, 60);
+            this.subtitleLabel.TabIndex = 3;
+            this.subtitleLabel.Text = "subtitleLabel";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(760, 400);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.troubleshootLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Boring Platformer";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label troubleshootLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+    }
+}
+
---
+++ BoringPlatformer/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace BoringPlatformer
+{
+    public partial class Form1 : Form
+    {
+        //player
+        Rectangle player = new Rectangle(290, 380, 20, 20);
+        const int playerXSpeed = 6;
+        int playerYSpeed = 0;
+
+        //platforms
+        List<Rectangle> platform = new List<Rectangle>();
+
+        //player control variables
+        bool wDown = false;
+        bool aDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        bool spaceDown = false;
+
+        //variable to track when the player is in the air
+        bool inAir = false;
+
+        //jumping stuff
+        //made up numbers: List<int> jumpYSpeed = new List<int>(new int[] { -15, -10, -6, -3, -1, 0, 0, 0, 1, 3, 6, 10, 15 });
+        //made up numbers 2.0: List<int> jumpYSpeed = new List<int>(new int[] { -30, -25, -20, -16, -12, -9, -6, -4, -2, -1, 0, 1, 2, 4, 6, 9, 12, 16, 20, 25, 30 });
+        //parabola: List<int> jumpYSpeed = new List<int>(new int[] {-36, -25, -16, -9, -4, -1, 0, 1, 4, 9, 16, 25, 36, 49, 64 });
+        List<int> jumpYSpeed = new List<int>(new int[] { -25, -18, -13, -9, -5, -2, -1, 0, 1, 2, 5, 9, 13, 18, 25, 32, 39, 45, 50, 54, 57, 59, 60, 60, 60, 60, 60, 60, 60, 60, 60 });
+
+        int jumpCounter = 0;
+        int score = 0;
+
+        //brush
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        //gamestate variable
+        string gameState = "waiting";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            scoreLabel.Visible = false;
+        }
+
+        public void GameInitialize()
+        {
+            titleLabel.Visible = false;
+            subtitleLabel.Visible = false;
+            scoreLabel.Visible = true;
+            scoreLabel.Text = "0";
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+            score = 0;
+
+            player.Location = new Point(290, 380);
+
+            platform.Add(new Rectangle (0, 360, 180, 20));
+            platform.Add(new Rectangle(260, 360, 180, 20));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Space:
+                    spaceDown = true;
+                    break;
+                case Keys.Enter:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        SoundPlayer confirmBeep = new SoundPlayer(Properties.Resources.confirmBeep);
+                        confirmBeep.Play();
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Space:
+                    spaceDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move the player
+            if (aDown == true && player.X > 0)
+            {
+                player.X -= playerXSpeed;
+            }
+            
+            if (dDown == true && player.X <= this.Width - player.Width)
+            {
+                player.X += playerXSpeed;
+            }
+
+            if (spaceDown == true && inAir == false)
+            {
+                player.Y += jumpYSpeed[0];
+                jumpCounter = 1;
+                inAir = true;
+            }
+            else if (inAir == true)
+            {
+                player.Y += jumpYSpeed[jumpCounter];
+                jumpCounter++;
+                //troubleshootLabel.Text = $"jumpCounter = {jumpCounter}";
+
+                for (int i = 0; i < platform.Count; i++)
+                {
+                    if (player.X + player.Width > platform[i].X && player.X < platform[i].X + platform[i].Width && player.Y >= platform[i].Y && player.Y + jumpYSpeed[jumpCounter] >= platform[i].Y)
+                    {
+                        player.Y = platform[i].Y - player.Height;
+                        inAir = false;
+                    }
+                }
+                if (player.Y > this.Height)
+                {
+                    gameState = "over";
+                    gameTimer.Enabled = false;
+                }
+            }
+            
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "waiting")
+            {
+                titleLabel.Text = "BORING PLATFORMER";
+                subtitleLabel.Text = "Press Enter to Start or Escape to Exit";
+            }
+            else if (gameState == "running")
+            {
+                //draw player
+                e.Graphics.FillRectangle(whiteBrush, player);
+
+                //draw platforms
+                for (int i = 0; i < platform.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, platform[i]);
+                }
+            }
+            else if (gameState == "over")
+            {
+                titleLabel.Text = "titleLabel";
+                subtitleLabel.Text = "subtitleLabel";
+
+                scoreLabel.Visible = false;
+                titleLabel.Visible = true;
+                subtitleLabel.Visible = true;
+            }
+        }
+    }
+}
